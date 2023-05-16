82db18d0c07a16a00a5f5002dfe3d133bbada701
manuelnones <manuelnones99@yahoo.com>
2023-05-16 15:05:41 +0200

    create model + migration  + seeder + controller

---
+++ app/Http/Controllers/ComicbookController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comicbook;
+use Illuminate\Http\Request;
+
+class ComicbookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Comicbook  $comicbook
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Comicbook $comicbook)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Comicbook  $comicbook
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Comicbook $comicbook)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Comicbook  $comicbook
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Comicbook $comicbook)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Comicbook  $comicbook
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Comicbook $comicbook)
+    {
+        //
+    }
+}
