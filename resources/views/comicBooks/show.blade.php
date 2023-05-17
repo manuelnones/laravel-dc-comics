507b5a1b22ddad05be64c7bf2713eb562bf7411d
manuelnones <manuelnones99@yahoo.com>
2023-05-17 16:23:36 +0200

    resize img comic in show

--- resources/views/comicBooks/show.blade.php
+++ resources/views/comicBooks/show.blade.php
@@ -4,20 +4,23 @@
 @section('content')
 
 <main class="text-center text-light">
-  <img src="{{$comicbook->thumb}}" alt="immagine della pasta">
+    <div class="big-img-comic">
+        <img src="{{$comicbook->thumb}}" alt="immagine della pasta">
+    </div>
 
-  <hr>
+    <hr>
 
-  <h1>{{$comicbook->series}}</h1>
+    <h1>{{$comicbook->series}}</h1>
 
-  <ul class="text-left">
-    <li>
-      Tipo: {{$comicbook->type}}
-    </li>
+    <ul class="text-left">
+        <li>
+        Tipo: {{$comicbook->type}}
+        </li>
+    </ul>
 
-  <p>
-    {{$comicbook->description}}
-  </p>
+    <p>
+        {{$comicbook->description}}
+    </p>
 </main>
 {{-- {{dd($comicbook)}} --}}
 
